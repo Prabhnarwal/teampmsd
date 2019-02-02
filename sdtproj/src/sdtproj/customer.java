6c4ec8ace3954aad8fa762667b5cd46f42374d5c
1894828 <1894828@E21057.CEGEPGIM.LCL>
2019-02-02 11:53:54 -0500

    customer

---
+++ sdtproj/src/sdtproj/customer.java
@@ -0,0 +1,14 @@
+
+
+package sdtproj;
+
+public class customer {
+     String name;
+     String address;
+     String email;
+     String customer_id;
+     double account_balance;
+     
+     
+  
+}
