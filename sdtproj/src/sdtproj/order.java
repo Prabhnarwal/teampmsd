c50f5f76a9f7fa2daa0f83217c29413662b3c98e
1894994 <1894994@E21058.CEGEPGIM.LCL>
2019-02-03 10:11:03 -0500

    order

--- sdtproj/src/sdtproj/order.java
+++ sdtproj/src/sdtproj/order.java
@@ -2,10 +2,14 @@ package sdtproj;
 
 import java.util.Date;
 
-public class order {
+public class order extends customer {
 
     String orderID;
     Date date;
     String customerName;
+
+    public order(int theID, String pass, Date rdate) {
+        super(theID, pass, rdate);
+    }
 
 }
