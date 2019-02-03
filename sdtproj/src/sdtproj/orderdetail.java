7fef31fa459eaebc92cc7117c50d78057f695acb
1894994 <1894994@E21058.CEGEPGIM.LCL>
2019-02-03 11:25:21 -0500

    orderdetail

--- sdtproj/src/sdtproj/orderdetail.java
+++ sdtproj/src/sdtproj/orderdetail.java
@@ -2,16 +2,63 @@ package sdtproj;
 
 import java.util.Date;
 
-public class orderdetail extends order {
+public class orderdetail  {
 
     String productID;
     String productName;
-    int quantitiy;
+    int quantity;
     double unitCost;
     double total;
 
-    public orderdetail(int theID, String pass, Date rdate) {
-        super(theID, pass, rdate);
+    void setproductID(String prID) {
+        productID = prID;
     }
 
+    void setproductName(String pname) {
+        productName = pname;
+    }
+
+    void setquantity(int quant) {
+        quantity = quant;
+    }
+
+    void setunitCost(Double ucost) {
+        unitCost = ucost;
+    }
+
+    void settotal(Double theTotal) {
+        total = theTotal;
+    }
+
+    public orderdetail(String prID, String pname, int quant, Double ucost, Double theTotal) {
+        productID = prID;
+        productName = pname;
+        quantity = quant;
+        unitCost = ucost;
+        total = theTotal;
+    }
+
+    String getproductID() {
+        return productID;
+    }
+
+    String getproductName() {
+        return productName;
+    }
+
+    int getquantity() {
+        return quantity;
+    }
+
+    Double getunitCost() {
+        return unitCost;
+    }
+
+    Double gettotal() {
+        return total;
+    }
+
+    void display1(String prID, String pname, int quant, Double ucost, Double theTotal) {
+        System.out.println(prID + " " + pname + " " + quant + " " + ucost + " " + theTotal);
+    }
 }
