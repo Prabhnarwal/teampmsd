dea899bbcb0d5db42e5c75798f71718532a4434a
Santhosh <santoshcheera007@gmail.com>
2019-02-03 12:24:52 -0500

    customer

--- sdtproj/src/sdtproj/customer.java
+++ sdtproj/src/sdtproj/customer.java
@@ -11,7 +11,49 @@ public class customer extends userclass {
     double accountBalance;
 
     public customer(int theID, String pass, Date rdate) {
+       
         super(theID, pass, rdate);
     }
 
+    void setName(String Name) {
+        name=Name ;
+    }
+
+    void setAddress(String Address) {
+         address=Address ;
+    }
+
+    void setEmail(String Email) {
+         email=Email ;
+    }
+
+    void setCustomerId(String CustomerId) {
+        customerId=CustomerId ;
+    }
+
+    void setAccountBalance(Double AccountBalance) {
+        accountBalance = AccountBalance;
+    }
+    
+     String getName(String Name) {
+        return name ;
+    }
+
+    String getAddress(String Address) {
+         return address;
+    }
+
+    String getEmail(String Email) {
+         return email;
+    }
+
+    String getCustomerId(String CustomerId) {
+        return customerId;
+    }
+
+    double getAccountBalance(Double AccountBalance) {
+        return accountBalance;
+    }
+    
+     
 }
