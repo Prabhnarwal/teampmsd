a080378ee6874835b2740cd0fd82a945b7906c2f
1894994 <1894994@E21058.CEGEPGIM.LCL>
2019-02-03 10:11:56 -0500

    userclass

--- sdtproj/src/sdtproj/userclass.java
+++ sdtproj/src/sdtproj/userclass.java
@@ -7,12 +7,38 @@ public class userclass {
     int userID;
     String password;
     Date registerDate;
-void setuserID(int theID)
-{
-    userID=theID;
-}
-public userclass(int theID)
-{
-    userID=theID;
-}
+
+    void setuserID(int theID) {
+        userID = theID;
+    }
+
+    void setpassword(String pass) {
+        password = pass;
+    }
+
+    void setregisterDate(Date rdate) {
+        registerDate = rdate;
+    }
+
+    public userclass(int theID, String pass, Date rdate) {
+        userID = theID;
+        password = pass;
+        registerDate = rdate;
+    }
+
+    int getuserID() {
+        return userID;
+    }
+
+    String getpassword() {
+        return password;
+    }
+
+    Date getregisterDate() {
+        return registerDate;
+    }
+
+    void display(int theID, String pass, Date rdate) {
+        System.out.println(theID + " " + pass + " " + rdate);
+    }
 }
