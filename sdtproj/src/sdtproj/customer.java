db7c23a9110007a5f8f35e3483b4424cc019e72d
1894994 <1894994@E21058.CEGEPGIM.LCL>
2019-02-03 10:10:40 -0500

    customer

--- sdtproj/src/sdtproj/customer.java
+++ sdtproj/src/sdtproj/customer.java
@@ -1,11 +1,17 @@
 package sdtproj;
 
-public class customer extends userclass{
+import java.util.Date;
+
+public class customer extends userclass {
 
     String name;
     String address;
     String email;
     String customerId;
     double accountBalance;
+
+    public customer(int theID, String pass, Date rdate) {
+        super(theID, pass, rdate);
+    }
 
 }
