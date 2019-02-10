9da8ad86790786a899997ff1ded7ae2c805d427d
1894994 <1894994@E21058.CEGEPGIM.LCL>
2019-02-10 10:07:36 -0500

    payment

--- team10/src/team10/payment.java
+++ team10/src/team10/payment.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package team10;
 
-/**
- *
- * @author 1894994
- */
 public class payment {
+
     float amount;
+    order o = new order();
 }
