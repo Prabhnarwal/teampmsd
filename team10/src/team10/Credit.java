d914db6676ab4a3cfc89668868916f572e59dc08
Santhosh <santoshcheera007@gmail.com>
2019-02-10 09:58:18 -0500

    credit

--- team10/src/team10/Credit.java
+++ team10/src/team10/Credit.java
@@ -3,12 +3,13 @@ package team10;
 
 import java.util.Date;
 
-public class Credit {
+public class Credit extends payment{
     
     String name;
     String type;
     
     Date expDate;
+     payment p = new payment();
     
     public void authorized()
     {
