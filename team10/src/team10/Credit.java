8dcb7837f5b977d55c35c16dc8899f89d9992818
Santhosh <santoshcheera007@gmail.com>
2019-02-10 09:54:30 -0500

    credit

---
+++ team10/src/team10/Credit.java
@@ -0,0 +1,17 @@
+
+package team10;
+
+import java.util.Date;
+
+public class Credit {
+    
+    String name;
+    String type;
+    
+    Date expDate;
+    
+    public void authorized()
+    {
+        System.out.println("authorized");
+    }
+}
